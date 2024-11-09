1caff5d8722d22e3c5eb948ffd922abd421f16e8
JERALD D <jeraldchan575@gmail.com>
2024-11-09 09:04:31 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -38,3 +38,21 @@ function App() {
 }
 
 export default App;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import App from './App.tsx';
+import outputs from '../amplify_outputs.json';
+import './index.css';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(outputs);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>
+)
